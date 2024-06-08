56fbd8b97004c1a2aaa23f1d30ddc36f57d209b6
Lord-Bambucha <lordbambycha93@gmail.com>
2024-06-09 01:26:47 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,33 +1,44 @@
 
-const modal = document.getElementById("modal");
-const closeBtn = document.querySelector(".modal__close-btn");
-const modalBtns = document.querySelectorAll(".js-modal");
+// const modal = document.getElementById("modal");
+// const closeBtn = document.querySelector(".modal__close-btn");
+// const modalBtns = document.querySelectorAll(".js-modal");
 
-(() => {
-    if (modalBtns.length > 0) {
-        modalBtns.forEach(el => el.addEventListener("click", onModalBtnClick));
-    }
-    closeBtn.addEventListener("click", onCloseBtnClick);
-})();
+// (() => {
+//     if (modalBtns.length > 0) {
+//         modalBtns.forEach(el => el.addEventListener("click", onModalBtnClick));
+//     }
+//     closeBtn.addEventListener("click", onCloseBtnClick);
+// })();
 
-function onModalBtnClick(e) {
-    e.preventDefault();
-    modal.showModal();
-    document.body.classList.toggle("modal-open");
-    document.addEventListener('click', handlerBackdropClick);
-}
+// function onModalBtnClick(e) {
+//     e.preventDefault();
+//     modal.showModal();
+//     document.body.classList.toggle("modal-open");
+//     document.addEventListener('click', handlerBackdropClick);
+// }
 
-function handlerBackdropClick(event) {
-    if (!event.target.classList.contains('modal__cont') && event.target.classList.contains('aaa')) {
-        modal.close();
-        document.removeEventListener('click', handlerBackdropClick);
-        document.body.classList.toggle("modal-open");
-    }
-}
+// function handlerBackdropClick(e) {
+//     if (!e.target.classList.contains('modal__cont') && e.target.classList.contains('aaa')) {
+//         modal.close();
+//         document.body.classList.toggle("modal-open");
+//         document.removeEventListener('click', handlerBackdropClick);
+//     }
+// }
 
-function onCloseBtnClick(e) {
-    e.preventDefault();
-    modal.close();
-    document.body.classList.toggle("modal-open");
-    document.removeEventListener('click', handlerBackdropClick);
-}
+// function onCloseBtnClick(e) {
+//     e.preventDefault();
+//     modal.close();
+//     document.body.classList.toggle("modal-open");
+//     document.removeEventListener('click', handlerBackdropClick);
+// }
+
+import MicroModal from 'micromodal';
+
+MicroModal.init({
+    // onShow: modal => console.info(`${modal.id} is shown`), // [1]
+    // onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    disableScroll: true, // [6]
+    awaitOpenAnimation: true,  // [8]
+    awaitCloseAnimation: true,  // [9]
+});
+
